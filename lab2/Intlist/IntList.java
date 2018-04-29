f4ee94edae079bf682db8e1a8d7e88bd61f1931b
Haversham <376305734@qq.com>
2018-04-29 21:09:30 +0800

    lab2 complete

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,8 +81,12 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        IntList L = A;
+        while(A.rest != null){
+            A = A.rest;
+        }
+        A.rest = B;
+        return L;
     }
 
     /**
@@ -90,8 +94,11 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A.rest == null){
+            return new IntList(A.first, B);
+        }else{
+            return new IntList(A.first, catenate(A.rest, B));
+        }
     }
 
 
